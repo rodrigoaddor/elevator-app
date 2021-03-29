e786b8805eaf3d6a6b51b1a3b7ce8cd3381bbda3
Rodrigo Addor <rodrigotladdor@gmail.com>
2021-03-29 15:04:48 -0300

    Basic funcionality, improved button design

---
+++ src/main.ts
@@ -0,0 +1,56 @@
+import { delay } from './utils'
+
+declare var floors: number
+const buildingHeight = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--building-height'))
+const floorHeight = buildingHeight / floors
+
+const waitTime = 1000
+
+const elevator = document.getElementById('elevator') as HTMLDivElement
+const queue: number[] = []
+let working = false
+
+document.querySelectorAll('button[data-floor]').forEach((element) => {
+  const button = element as HTMLButtonElement
+  button.addEventListener('click', async (event) => {
+    if (event.target instanceof Element) {
+      const button = event.target as HTMLButtonElement
+      const floor = parseInt(button.dataset.floor!)
+      const currentFloor = parseInt(elevator.dataset.floor ?? '0')
+
+      if (currentFloor != floor && !queue.includes(floor)) {
+        queue.push(floor)
+        button.classList.remove('button-outline')
+
+        work()
+      }
+    }
+  })
+})
+
+const work = async () => {
+  if (working) return
+  working = true
+
+  do {
+    const floor = queue[0]
+    const currentFloor = parseInt(elevator.dataset.floor ?? '0')
+
+    console.log(`Going to ${floor}`)
+
+    elevator.dataset.floor = `${floor}`
+    await elevator.animate([{ transform: `translateY(${-floorHeight * currentFloor}px)` }, { transform: `translateY(${-floorHeight * floor}px)` }], {
+      duration: 500 * Math.abs(floor - currentFloor),
+      fill: 'forwards',
+      easing: 'ease-in-out',
+    }).finished
+
+    queue.shift()
+    document.querySelectorAll(`button[data-floor="${floor}"]`).forEach((button) => button.classList.add('button-outline'))
+
+    await delay(waitTime)
+  } while (queue.length > 0)
+
+  console.log('Done?' + queue)
+  working = false
+}
